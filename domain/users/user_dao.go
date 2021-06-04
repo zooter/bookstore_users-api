2b0d18abc3c2e8a9ca527b093becd6341b142ec0
Jayesh Pathade <jayeshpathade@gmail.com>
2021-06-04 14:32:55 +0530

    Inserting to DB

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -8,6 +8,10 @@ import (
 	"github.com/zooter/bookstore_users-api/utils/errors"
 )
 
+const(
+	queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES (?, ? ,? ,?)"
+)
+
 var (
 	userDB = make(map[int64]*User_DTO)
 )
@@ -31,6 +35,25 @@ func (user *User_DTO) Get() *errors.RestErr {
 }
 
 func (user *User_DTO) Save() *errors.RestErr {
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return  errors.NewInternalServerError(err.Error())
+	}
+	defer stmt.Close()
+
+	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+
+	if err != nil {
+		return  errors.NewInternalServerError(fmt.Sprintf("Error when trying to save user: %s", err.Error()))
+	}
+
+	userId, err := insertResult.LastInsertId();
+	if err != nil {
+		return  errors.NewInternalServerError(fmt.Sprintf("Error when trying to save user: %s", err.Error()))
+	}
+
+	user.Id = userId
+
 	if userDB[user.Id] != nil {
 		return errors.NewBadRequestError(fmt.Sprintf("User %d already exists", user.Id))
 	}
